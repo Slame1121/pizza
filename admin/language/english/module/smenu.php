35d0b4cfed45a911c6e21ab74cfb7c8bc8c40938
Oleg <slame1121@gmail.com>
2017-08-30 23:48:48 +0300

    commit

---
+++ admin/language/english/module/smenu.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']       = 'Simple Menu';
+
+// Text
+$_['text_edit']           = 'Edit module sMenu';
+$_['text_success']        = 'Success: You have modified module sMenu!';
+// Entry
+$_['entry_name']          = 'Module name:';
+$_['entry_menu']          = 'Select menu:';
+$_['entry_status']        = 'Status:';
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module sMenu!';
+//Button
+$_['button_control']      = 'Go to the menu control';
+?>
